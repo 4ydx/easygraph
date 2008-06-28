dca53772fcaccdff4a108784542504a6e8fea864
mojowings <mojowings@c4cc5456-6250-0410-a01c-1b7e6a35f18a>
2008-06-28 21:56:29 +0000

    Added preliminary work to actually draw the graph.  Doesn't do any yet though :D

--- trunk/Graphing.h
+++ trunk/Graphing.h
@@ -7,27 +7,30 @@
 
 #include "ui_Graphing.h"
 #include "ConstantsModel.h"
+#include "Point.h"
 
 #include <QObject>
 
 class Graphing : public QObject
 {
 	Q_OBJECT
-	
+
 public:
 	Graphing();
 	virtual ~Graphing();
-	
+
 	void initialize(QMainWindow &main);
-	
+
 public slots:
 	void EvaluateEquation();
 	void AddConstantsModelPoint();
 	void ClearConstantsModel();
-	
+
 public:
 	Ui::MainWindow mainWindow;
 	ConstantsModel model;
+
+	QList<Point> graphPoints;
 };
 
 #endif /*GRAPHING_H_*/
