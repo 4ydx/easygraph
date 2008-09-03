ce422d48870cfd73322b3222cb48848f7a839143
mojowings <mojowings@c4cc5456-6250-0410-a01c-1b7e6a35f18a>
2008-09-03 14:01:47 +0000

    Improved on moving the graph about.

--- Point.cpp
+++ Point.cpp
@@ -1,13 +1,14 @@
 #include "Point.h"
 
 Point::Point()
+  :X(0), Y(0), Z(0)
 {
-	X = Y = Z = 0;
+	this->dimensions = D2;
 }
 
 Point::Point(PointType dimensions)
+  :X(0), Y(0), Z(0)
 {
-	X = Y = Z = 0;
 	this->dimensions = dimensions;	
 }
 
